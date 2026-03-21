9f77cda082d5e96d3739362313059fdf89ba5ed6
SerbianGhost <serbianghost816@gmail.com>
2026-03-21 13:05:52 +0500

    chore:basic mvc structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 function App() {
-  return <>
-  <div className="flex">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi veniam harum neque obcaecati odit dolorem voluptatum repellendus, sit vel aperiam labore. Distinctio unde eos rem voluptatum et quidem numquam ratione?</p>
-    <p>Perferendis est cum iusto repellat suscipit qui maxime nesciunt nisi non. Veniam ratione quas, dolores, ullam aut quia temporibus error consequuntur sit, veritatis ducimus neque corrupti illo quasi. Animi, repellat.</p>
-  </div>
-  </>
+  return <>Приложение работает!</>
 }
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tanstackRouter from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@views': path.resolve(__dirname, './src/views'),
+      '@controllers': path.resolve(__dirname, './src/controllers'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 })
