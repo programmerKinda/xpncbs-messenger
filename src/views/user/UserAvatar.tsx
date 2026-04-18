20ba5b0de7fabc77b265801e7cb57e66cd1f5f9b
SerbianGhost <serbianghost816@gmail.com>
2026-04-18 19:22:51 +0500

    fix userAvatar letters offset

--- src/views/user/UserAvatar.tsx
+++ src/views/user/UserAvatar.tsx
@@ -5,17 +5,24 @@ export default function UserAvatar({ name, avatarURL }: { name: string; avatarUR
   const colorHash = new ColorHash()
   const nameArray = name.split(' ')
   const colorMain = colorHash.hex(name)
-  let lighterColor = chroma(colorMain).brighten(1.2).hex()
-  let darkenColor = chroma(colorMain).darken(0.5).hex()
-  
+  const lighterColor = chroma(colorMain).brighten(1.2).hex()
+  const darkenColor = chroma(colorMain).darken(0.5).hex()
+  const styles = avatarURL ? {} : { backgroundColor: colorMain, border: `2px solid ${darkenColor}` }
+  const letters = `${nameArray[0][0]}${nameArray[1][0]}`
+  const isAllLower = letters === letters.toLowerCase()
+
+  const offset = isAllLower ? '-0.06em' : '0'
+
   return (
-    <div className="user-avatar" style={{ backgroundColor: colorMain,  border: `2px solid ${darkenColor}` }}>
+    <div className="user-avatar" style={styles}>
       {avatarURL ? (
         <img src={avatarURL} alt="" className="user-avatar__img" />
       ) : (
-        <span className="user-avatar__name" style={{color:lighterColor}}>
-          {nameArray[0][0]}
-          {nameArray[1][0]}
+        <span
+          className="user-avatar__name"
+          style={{ color: lighterColor, transform: `translateY(${offset})` }}
+        >
+          {letters}
         </span>
       )}
     </div>
