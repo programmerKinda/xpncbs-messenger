7eac8c5342612939517b4ad2706b0957285e750c
XPNCBS <d6412248@gmail.com>
2026-07-17 18:32:36 +0300

    вносим пачти

--- src/utils/sidebarElements.tsx
+++ src/utils/sidebarElements.tsx
@@ -7,8 +7,9 @@ export const sidebarElements = [
   {
     icon: <BiSolidMessageDetail size={35} />,
     text: 'Чаты',
+    path: '/'
   },
-  { icon: <FaUsers size={35} />, text: 'Контакты' },
-  { icon: <FaPhoneVolume size={25} />, text: 'Звонки' },
-  { icon: <IoMdSettings size={35} />, text: 'Настройки' },
+  { icon: <FaUsers size={35} />, text: 'Контакты',path: '/contacts' },
+  { icon: <FaPhoneVolume size={25} />, text: 'Звонки',path: '/calls' },
+  { icon: <IoMdSettings size={35} />, text: 'Настройки',path: '/settings' },
 ]
--- src/views/sidebar/SidebarItem.tsx
+++ src/views/sidebar/SidebarItem.tsx
@@ -1,10 +1,15 @@
+
+import { useNavigate,useLocation } from "react-router-dom"
 interface SidebarItemProps {
   icon: React.ReactNode
   text: string
+  path: string
 }
-export const SideBarItem: React.FC<SidebarItemProps> = ({ icon, text }) => {
+export const SideBarItem: React.FC<SidebarItemProps> = ({ icon, text, path }) => {
+  const navigate = useNavigate()
+  const {pathname} = useLocation()
   return (
-    <li className="sidebar__item-container">
+    <li className={`sidebar__item-container ${pathname === path ? 'active' : ''}`} onClick={()=>{navigate(path)}}>
       <div className="sidebar__item" title={text}>
         {icon}
         <span className="sidebar__item-text">{text}</span>
